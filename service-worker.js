72966857e13dbdf5aaf2812428619d850ebb18cf
alexlefk <alex.lefkidis@gmail.com>
2021-02-01 12:53:00 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.a4726047c88bc5f7be933d5d8165da85.js"
+  "/react-portfolio/precache-manifest.812dd0150a553d4729c7ec69eaaafb3f.js"
 );
 
 self.addEventListener('message', (event) => {
